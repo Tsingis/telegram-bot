03905fa97c350a09a6b626dd25db4b59772e6107
Jesse Kela <jessekkela@gmail.com>
2020-01-19 11:18:03 +0200

    Removed WC3_data

--- commands.py
+++ commands.py
@@ -27,7 +27,7 @@ def command_response(text, bot, chat_id):
         f1_cmds = ["/f1info", "/f1results", "/f1standings"]
         nhl_cmds = ["/nhlinfo", "/nhlresults", "/nhlstandings",
                     "/nhlfinns", "/nhlplayoffs", "/nhlplayer <player name>"]
-        other_cmds = ["/wc3info", "/weather <location>", "/search <keyword>"]
+        other_cmds = ["/weather <location>", "/search <keyword>"]
 
         msg = ("*F1 commands:*\n" + "\n".join(f1_cmds) + "\n"
                + "*NHL commands:*\n" + "\n".join(nhl_cmds) + "\n"
@@ -132,15 +132,6 @@ def command_response(text, bot, chat_id):
             msg = stats + f"\n[Details]({url})"
             if (player_contract is not None):
                 msg = msg + "\nContract:\n" + player_contract + f"\n[Details]({contract_url})"
-        else:
-            msg = "Not available"
-        send_message(msg, chat_id, bot)
-
-    # Warcraft 3 upcoming events
-    if (text == "/wc3info"):
-        events_info = WC3_data.get_events()
-        if (events_info is not None):
-            msg = "*Upcoming events:*\n" + "\n".join(events_info)
         else:
             msg = "Not available"
         send_message(msg, chat_id, bot)
