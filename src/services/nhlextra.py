79724f58137bd57443fae08e90992e53a460788c
Jesse Kela <jessekkela@gmail.com>
2021-05-11 19:58:38 +0300

    Fixed wrongly named argument after recent change

--- src/services/nhlextra.py
+++ src/services/nhlextra.py
@@ -16,7 +16,7 @@ class NHLExtra(NHLBase):
         name = name.replace(" ", "-").replace("\'", "").lower()
         url = f"https://www.capfriendly.com/players/{name}"
         try:
-            soup = set_soup(url, target_encoding="utf-8")
+            soup = set_soup(url, targetEncoding="utf-8")
 
             # Find table of current contract
             table = soup.find("table", {"class": "cntrct fixed tbl"})
