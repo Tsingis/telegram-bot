0ab00b70719bfdfe9f79aa351eaba51e4b008cdb
Jesse Kela <jessekkela@gmail.com>
2021-05-31 23:11:18 +0300

    Improved bracket logic to look for winners of previous rounds

--- src/services/nhladvanced.py
+++ src/services/nhladvanced.py
@@ -265,12 +265,14 @@ class NHLAdvanced(NHLBase):
                            for team in teams if team["seed"]["isTop"])
             bottomTeam = next(team["team"]["name"]
                               for team in teams if not team["seed"]["isTop"])
+            winner = get_winner_from_teams(teams)
             return {
                 "matchup": {
                     "top": self.teams[topTeam]["shortName"],
                     "bottom": self.teams[bottomTeam]["shortName"]
                 },
-                "status": series["currentGame"]["seriesSummary"]["seriesStatusShort"]
+                "status": series["currentGame"]["seriesSummary"]["seriesStatusShort"],
+                "winner": winner
             }
 
         def insert_team_to_bracket(team, location):
@@ -285,62 +287,104 @@ class NHLAdvanced(NHLBase):
             imgText.text(location, status, anchor="mm",
                          font=font, fill=(0, 0, 0))
 
+        def get_winner_from_teams(teams):
+            for team in teams:
+                if team["seriesRecord"]["wins"] == 4:
+                    return self.teams[team["team"]["name"]]["shortName"]
+
         try:
             data = self.get_playoffs()
             bracket = dict()
             for round in data["rounds"]:
                 for series in round["series"]:
-                    if "matchupTeams" in series:
-                        bracket[series["seriesCode"]] = get_series_data(series)
+                    if "matchupTeams" in series and series["round"]["number"] > 0:
+                        bracket[series["seriesCode"]
+                                ] = get_series_data(series)
 
             # Open blank playoff bracket
             imgBracket = Image.open("static/playoffs_template.png")
 
             # Locations for logos in bracket
             # Round 1
-            bracket["A"]["location"] = {
-                "top": (1175, 50), "bottom": (1175, 185)}
-            bracket["B"]["location"] = {
-                "top": (1175, 280), "bottom": (1175, 405)}
-            bracket["C"]["location"] = {
-                "top": (1175, 505), "bottom": (1175, 625)}
-            bracket["D"]["location"] = {
-                "top": (1175, 730), "bottom": (1175, 865)}
-            bracket["E"]["location"] = {
-                "top": (15, 505), "bottom": (15, 625)}
-            bracket["F"]["location"] = {
-                "top": (15, 730), "bottom": (15, 865)}
-            bracket["G"]["location"] = {
-                "top": (15, 50), "bottom": (15, 185)}
-            bracket["H"]["location"] = {
-                "top": (15, 280), "bottom": (15, 405)}
+            if ("A" in bracket.keys()):
+                bracket["A"]["location"] = {
+                    "top": (1175, 50), "bottom": (1175, 185)}
+            if ("B" in bracket.keys()):
+                bracket["B"]["location"] = {
+                    "top": (1175, 280), "bottom": (1175, 405)}
+            if ("C" in bracket.keys()):
+                bracket["C"]["location"] = {
+                    "top": (1175, 505), "bottom": (1175, 625)}
+            if ("D" in bracket.keys()):
+                bracket["D"]["location"] = {
+                    "top": (1175, 730), "bottom": (1175, 865)}
+            if ("E" in bracket.keys()):
+                bracket["E"]["location"] = {
+                    "top": (15, 505), "bottom": (15, 625)}
+            if ("F" in bracket.keys()):
+                bracket["F"]["location"] = {
+                    "top": (15, 730), "bottom": (15, 865)}
+            if ("G" in bracket.keys()):
+                bracket["G"]["location"] = {
+                    "top": (15, 50), "bottom": (15, 185)}
+            if ("H" in bracket.keys()):
+                bracket["H"]["location"] = {
+                    "top": (15, 280), "bottom": (15, 405)}
 
             # Round 2
             if ("I" in bracket.keys()):
                 bracket["I"]["location"] = {
                     "top": (1000, 350), "bottom": (1000, 120)}
+                bracket["I"]["matchup"] = {
+                    "top": bracket["B"]["winner"],
+                    "bottom": bracket["A"]["winner"]
+                }
             if ("J" in bracket.keys()):
                 bracket["J"]["location"] = {
                     "top": (1000, 800), "bottom": (1000, 570)}
+                bracket["J"]["matchup"] = {
+                    "top": bracket["D"]["winner"],
+                    "bottom": bracket["C"]["winner"]
+                }
             if ("K" in bracket.keys()):
                 bracket["K"]["location"] = {
                     "top": (185, 800), "bottom": (185, 570)}
+                bracket["K"]["matchup"] = {
+                    "top": bracket["F"]["winner"],
+                    "bottom": bracket["E"]["winner"]
+                }
             if ("L" in bracket.keys()):
                 bracket["L"]["location"] = {
                     "top": (185, 350), "bottom": (185, 120)}
+                bracket["L"]["matchup"] = {
+                    "top": bracket["H"]["winner"],
+                    "bottom": bracket["G"]["winner"]
+                }
 
             # Round 3
             if ("M" in bracket.keys()):
                 bracket["M"]["location"] = {
                     "top": (835, 235), "bottom": (835, 675)}
+                bracket["M"]["matchup"] = {
+                    "top": bracket["I"]["winner"],
+                    "bottom": bracket["J"]["winner"]
+                }
             if ("N" in bracket.keys()):
                 bracket["N"]["location"] = {
                     "top": (355, 235), "bottom": (355, 675)}
+                bracket["N"]["matchup"] = {
+                    "top": bracket["L"]["winner"],
+                    "bottom": bracket["K"]["winner"]
+                }
 
             # Round 4, Stanley cup final
             if ("O" in bracket.keys()):
                 bracket["O"]["location"] = {
                     "top": (665, 450), "bottom": (530, 450)}
+                bracket["O"]["matchup"] = {
+                    "top": bracket["M"]["winner"],
+                    "bottom": bracket["N"]["winner"]
+                }
 
             # Insert teams into bracket
             for key, value in bracket.items():
