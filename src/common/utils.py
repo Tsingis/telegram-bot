da0dca079ee7111da1467aba816b61c42faef706
Jesse Kela <jessekkela@gmail.com>
2023-01-15 11:50:32 +0200

    Remove logging from utils

--- src/common/utils.py
+++ src/common/utils.py
@@ -3,62 +3,37 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from dateutil import tz
-from .logger import logging
 
 
-logger = logging.getLogger(__name__)
-
-
-def get(url, params={}):
-    """
-    HTTP GET request with or without parameters
-    """
-    try:
-        if params:
-            res = requests.get(url, params)
-        else:
-            res = requests.get(url)
-        if res.status_code == 200:
-            return res
-        res.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.exception(f"Error getting data with url {url}")
+def get(url, params=None):
+    if params:
+        res = requests.get(url, params)
+    else:
+        res = requests.get(url)
+    if res.status_code == 200:
+        return res
+    res.raise_for_status()
 
 
 def set_soup(url, target_encoding="latin-1"):
-    """
-    Set soup for given url
-    """
-    try:
-        res = get(url)
-        text = res.text.encode(target_encoding)
-        return BeautifulSoup(text, "html.parser")
-    except Exception:
-        logger.exception(f"Error setting soup with url {url}")
+    res = get(url)
+    text = res.text.encode(target_encoding)
+    return BeautifulSoup(text, "html.parser")
 
 
 def find_first_int(strings):
-    """
-    Finds first integer in list of strings
-    """
     for string in strings:
         if string.strip().isdigit():
             return int(string)
 
 
 def find_first_word(strings):
-    """
-    Finds first word in list of strings
-    """
     for string in strings:
         if string.strip().isalpha():
             return string
 
 
 def convert_timezone(date, source_tz=None, target_tz=None):
-    """
-    Convert date between timezones
-    """
     if source_tz is None:
         source_tz = tz.tzutc()
     else:
@@ -89,17 +64,11 @@ def remove_texts(text, texts_to_remove):
 
 
 def escape_special_chars(text):
-    """
-    Escapes MarkdownV2 engine special characters
-    """
     escape_chars = r"_*[]()~`>#+-=|{}.!"
     return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
 
 
 def format_number(number):
-    """
-    Formats floating number without insignificant trailing zeroes
-    """
     return f"{number:g}"
 
 
