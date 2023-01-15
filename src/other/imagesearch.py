fd28a75dcb0d09b32e42868296454f26cc77b05e
Jesse Kela <jessekkela@gmail.com>
2023-01-15 11:45:04 +0200

    Refactor imagesearch

--- src/other/imagesearch.py
+++ src/other/imagesearch.py
@@ -1,5 +1,5 @@
 import os
-from random import choice
+import random
 from ..common.logger import logging
 from ..common.utils import get
 
@@ -17,17 +17,6 @@ class ImageSearch:
 
     # Search image url with given keyword
     def get_random_image(self, keyword):
-        try:
-            data = self._get_data(keyword)
-            if "items" in data:
-                images = [result["link"] for result in data["items"]]
-                image = choice(images)
-                return image
-        except Exception:
-            logger.exception(f"Error getting image with keyword {keyword}")
-
-    # Get image data
-    def _get_data(self, keyword):
         url = "https://customsearch.googleapis.com/customsearch/v1"
         params = {
             "key": self.GOOGLE_API_KEY,
@@ -35,7 +24,14 @@ class ImageSearch:
             "searchType": "image",
             "fileType": "jpg",
             "hl": self.REGION.lower(),
+            "gl": self.REGION.lower(),
             "q": keyword,
         }
-        data = get(url, params).json()
-        return data
+        try:
+            data = get(url, params).json()
+            if "items" in data:
+                images = [result["link"] for result in data["items"]]
+                image = random.choice(images)
+                return image
+        except Exception:
+            logger.exception(f"Error getting image with keyword {keyword}")
