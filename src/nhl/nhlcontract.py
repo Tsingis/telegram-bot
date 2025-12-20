8812abc3b90cd43cd5453ab7c0862c159aa2b311
Jesse Kela <jessekkela@gmail.com>
2025-12-20 20:46:39 +0200

    Migrate from beautifulsoup to parsel

--- src/nhl/nhlcontract.py
+++ src/nhl/nhlcontract.py
@@ -3,7 +3,7 @@ from ..common.utils import (
     escape_special_chars,
     format_as_header,
     format_as_url,
-    set_soup,
+    set_selector,
 )
 from ..common.logger import logging
 
@@ -18,25 +18,35 @@ class NHLContract(NHLBase):
     def get(self, name):
         url = f"""{self.contract_base_url}/{name.replace(" ", "-").replace("'", "").lower()}"""
         try:
-            soup = set_soup(url, target_encoding="utf-8")
-            tables = soup.find_all(
-                "table", {"class": ["min-w-full bg-white dark:bg-gray-800 border"]}
+            selector = set_selector(url, target_encoding="utf-8")
+
+            tables = selector.xpath(
+                "//table[contains(@class, 'min-w-full') " "and contains(@class, 'bg-white')]"
             )
-            if tables is None:
+
+            if not tables:
                 logger.info(f"Contract table not found for player {name}")
                 return
+
             for table in tables:
-                rows = table.find_all("tr")
-                contract_rows = [
-                    row.find_all("td") for row in rows[1:-1]
-                ]  # Skip header and total rows
-                data = [
-                    {
-                        "season": cols[0].text.replace("-", "20"),
-                        "capHit": cols[2].text,
-                    }
-                    for cols in contract_rows
-                ]
+                rows = table.xpath(".//tr")[1:-1]  # skip header & total rows
+
+                data = []
+                for row in rows:
+                    cols = row.xpath(".//td")
+                    if len(cols) < 3:
+                        continue
+
+                    season = cols[0].xpath("string()").get().replace("-", "20")
+                    cap_hit = cols[2].xpath("string()").get()
+
+                    data.append(
+                        {
+                            "season": season,
+                            "capHit": cap_hit,
+                        }
+                    )
+
                 contract = next(
                     (
                         {
@@ -48,10 +58,10 @@ class NHLContract(NHLBase):
                     ),
                     None,
                 )
-                if contract is None:
-                    continue
 
-                return {"contract": contract, "url": url}
+                if contract:
+                    return {"contract": contract, "url": url}
+
         except Exception:
             logger.exception(f"Error getting player contract for player {name}")
 
