f4648748860d3fc04a1110cee2ff0889b9446540
Jesse Kela <jessekkela@gmail.com>
2026-07-04 10:48:01 +0300

    Change logic when checking against new season

--- src/nhl/nhlbase.py
+++ src/nhl/nhlbase.py
@@ -14,7 +14,7 @@ class NHLBase:
         self.target_timezone = "Europe/Helsinki"
         self.date = convert_timezone(dt=date, target_tz=self.target_timezone)
         year = self.date.year
-        self.season = f"{year - 1}{year}" if self.date.month < 7 else f"{year}{year + 1}"
+        self.season = f"{year - 1}{year}" if self.date.month < 10 else f"{year}{year + 1}"
         path = Path(__file__).resolve().parent.parent / "static"
         with open(os.path.join(path, "nhl_teams.json"), "r") as f:
             teams = json.load(f)
